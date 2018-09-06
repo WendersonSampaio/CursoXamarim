497a5e18a99cd25d46a6cfa014d003505bde64ba
Wenderson Sampaio <wcwsampaio@msn.com>
2018-09-06 01:57:49 -0300

    app_01 concluído com a configuração do teclado para apresentar apenas caracteres numéricos

---
+++ Secao5/App1_ConsultarCEP/App1_ConsultarCEP/App1_ConsultarCEP/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using App1_ConsultarCEP.Servico.Modelo;
+using App1_ConsultarCEP.Servico;
+
+namespace App1_ConsultarCEP
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            btBuscar.Clicked += BuscarCEP;
+        }
+
+        private void BuscarCEP(object sender, EventArgs args)
+        {
+            string cep = txtCEP.Text.Trim();
+            Endereco end = ViaCEP_Servico.BuscaEndereco(cep);
+
+            lblResultado.Text = string.Format("Endereço: \n {0}, \n {1}, \n {2}, \n {3}", end.Logradouro,end.Complemento, end.Localidade, end.Bairro);
+
+            //"\r\n"
+        }
+    }
+}
---
+++ Secao5/App1_ConsultarCEP/App1_ConsultarCEP/App1_ConsultarCEP/Servico/ViaCEP_Servico.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using App1_ConsultarCEP.Servico.Modelo;
+using Newtonsoft.Json;
+
+namespace App1_ConsultarCEP.Servico
+{
+    public class ViaCEP_Servico
+    {
+        private static string Endereco_URL = "http://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscaEndereco(string cep)
+        {
+            string novoEnderecoURL = string.Format(Endereco_URL, cep);
+
+            WebClient wc = new WebClient();
+
+            string conteudo = wc.DownloadString(novoEnderecoURL);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(conteudo);
+
+            return end;
+                                 
+        }
+
+    }
+}
